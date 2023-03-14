7f674b4ad65cf3e26a9e053af8d80304b4ca69c3
Aarti Tak <takaarti10@gmail.com>
2023-03-14 14:50:28 +0530

    calculator

---
+++ Calculator.js
@@ -0,0 +1,66 @@
+
+// let outputValue=document.querySelector('.output-value');
+// console.log(outputValue);
+//     let btn=document.querySelectorAll('#btn1,#btn2,#btn3,#btn4,#btn5,#btn6,#btn7,#btn8,#btn9,#btn10,#btn11,#btn12,#btn13,#btn14,#btn15,#btn16,#btn17,#btn18,#btn19,#btn20,#btn21,#btn22,#btn23,#final-output-btn');
+//     btn.forEach(elem=>elem.addEventListener("click",function(e){
+//      let id=e.target.id;
+//      let ele=document.getElementById(id);
+//      let a=ele.textContent;
+//      a=parseInt(a)
+//      console.log(typeof a);
+//      if(typeof a=='number')
+//      {
+//      console.log("ele value",a);
+//      }
+//      outputValue.textContent+=ele.textContent;      
+  
+//      let outputBtn=document.getElementById('final-output-btn');
+//      outputBtn.addEventListener("click",function(){
+      
+     
+//          if(ele.textContent=='+')
+//          {
+//             outputValue.textContent=ele.textContent + outputValue.textContent;
+//          }
+//      })
+//     }))
+
+
+
+//     let clearBtn=document.getElementById('btn5');     
+//     clearBtn.addEventListener("click",function(e){
+//       outputValue.textContent=0;
+      
+//     })
+   
+
+function calculate(result)
+{
+  if(result=="AC")
+  {
+    console.log("clcl");
+    let val=" ";
+    form.displayResult.value=val;
+   
+  }
+  form.displayResult.value=form.displayResult.value+result;
+}
+function clear()
+{
+  console.log("clesd");
+}
+   
+    
+   
+    
+ 
+   
+
+
+  
+
+
+
+  
+
+
